c2f430cd00a61471519c295326f4f0fba277184e
Abdellah <abdallahkaba98@gmail.com>
2024-04-06 01:04:13 +0000

    upload-post-image

--- index.js
+++ index.js
@@ -3,10 +3,29 @@ const postRoutes = require("./routes/posts.js");
 const userRoutes = require("./routes/users.js");
 const authRoutes = require("./routes/auth.js");
 const cookieParser = require('cookie-parser')
+const multer = require('multer')
 
 const app = express()
 app.use(cookieParser())
 app.use(express.json())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, Date.now()+file.originalname)
+    }
+  })
+  
+ 
+
+const upload = multer({ storage })
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    const file = req.file
+    res.status(200).json(file.filename)
+})
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
